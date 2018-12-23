b8fe9032bfa261f208cecd1e796d33bc46f82a42
SpammmerBots <44922126+SpammmerBots@users.noreply.github.com>
2018-12-23 03:47:50 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -7538,19 +7538,19 @@ client.on("ready", () => {
 
 client.on("ready", async  => {
 setInterval(function(){
-client.channels.find('id', '526360905211052042').setName("W");
-client.channels.find('id', '526360905211052042').setName("We");
-client.channels.find('id', '526360905211052042').setName("Wel");
-client.channels.find('id', '526360905211052042').setName("Welc");
-client.channels.find('id', '526360905211052042').setName("Welco");
-client.channels.find('id', '526360905211052042').setName("Welcom");
-client.channels.find('id', '526360905211052042').setName("Welcome");
-client.channels.find('id', '526360905211052042').setName("Welcome T");
-client.channels.find('id', '526360905211052042').setName("Welcome To");
-client.channels.find('id', '526360905211052042').setName("Welcome To M");
-client.channels.find('id', '526360905211052042').setName("Welcome To Ma");
-client.channels.find('id', '526360905211052042').setName("Welcome To Mal");
-client.channels.find('id', '526360905211052042').setName("Welcome To Mal S");
+client.channels.find('id', '526363779106209793').setName("W");
+client.channels.find('id', '526363779106209793').setName("We");
+client.channels.find('id', '526363779106209793').setName("Wel");
+client.channels.find('id', '526363779106209793').setName("Welc");
+client.channels.find('id', '526363779106209793').setName("Welco");
+client.channels.find('id', '526363779106209793').setName("Welcom");
+client.channels.find('id', '526363779106209793').setName("Welcome");
+client.channels.find('id', '526363779106209793').setName("Welcome T");
+client.channels.find('id', '526363779106209793').setName("Welcome To");
+client.channels.find('id', '526363779106209793').setName("Welcome To M");
+client.channels.find('id', '526363779106209793').setName("Welcome To Ma");
+client.channels.find('id', '526363779106209793').setName("Welcome To Mal");
+client.channels.find('id', '526363779106209793').setName("Welcome To Mal S");
 client.channels.find('id', '526360905211052042').setName("Welcome To Mal Sh");
 client.channels.find('id', '526360905211052042').setName("Welcome To Mal Sho");
 client.channels.find('id', '526360905211052042').setName("Welcome To Mal Shop");
