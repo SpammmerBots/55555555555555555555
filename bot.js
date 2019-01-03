373a9489f7feb395ddf7db1bf2f707218aba6fdf
SpammmerBots <44922126+SpammmerBots@users.noreply.github.com>
2019-01-03 05:24:09 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -1115,7 +1115,7 @@ client.on("message", (message) => {
     }
  
  
-  if (message.content.startsWith("g!...close")) {
+  if (message.content.startsWith("g!..close")) {
         if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
  
        message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`g!confirm\`. This will time out in 10 seconds and be cancelled.`)
@@ -1192,7 +1192,7 @@ if (message.content.toLowerCase().startsWith(prefix + `close`)) {
 
     message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`g!confirm\`. This will time out in 10 seconds and be cancelled.`)
     .then((m) => {
-      message.channel.awaitMessages(response => response.content === '-confirm', {
+      message.channel.awaitMessages(response => response.content === 'g!confirm', {
         max: 1,
         time: 10000,
         errors: ['time'],
